c679b1d8d5b6195fb696c08a68c5416125393500
nonebula <147166151+nonebula@users.noreply.github.com>
2023-12-15 20:15:18 +0000

    Added notes and to do. Attempted 5 day forecast but not complete. Some styling and lots of indecision.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,16 @@
 
 var APIKey = "6be34685c16ee0bfc0d0c732f2d8ba7c";
 var cityInput = $("#search-input");
-var APICallGeocoding = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&appid=" + APIKey;
+var APICallGeocoding = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&appid=" + APIKey + "&units=metric";
 
 var resultsToday = $("#today")
+var resultsFuture = $("#forecast")
 
 var cities = ["London", "Bogota", "New York", "Seoul"];
 
 // Displays city info
 function displayCityInfo(city) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=metric";
 
         fetch(queryURL)
             .then(function (response) {
@@ -23,14 +24,15 @@ function displayCityInfo(city) {
                 resultsToday.empty();
 
                 // H2 for City Name
-                var cityName = $("<h2>").text(data.name);
+                var cityName = $("<h2>").text(data.name); //today's date needs to be added here
+                //image thumbnail should be added here
                 resultsToday.append(cityName);
                 // P for description, temp, wind, humidity
                 var todayDesc = $("<p>").text("Description: " + data.weather[0].description);
                 resultsToday.append(todayDesc);
-                var fahrenheitTemp = data.main.temp;
-                var celsiusTemp = (fahrenheitTemp - 273.15);
-                var todayTemp = $("<p>").text("Temperature: " + celsiusTemp.toFixed(2) + "°C");
+                // var fahrenheitTemp = data.main.temp;
+                // var celsiusTemp = (fahrenheitTemp - 273.15);
+                var todayTemp = $("<p>").text("Temperature: " + data.main.temp.toFixed(2) + "°C");
                 resultsToday.append(todayTemp);
                     // var celsiusTemp = data.main.temp - 273.15
                 var todayWind = $("<p>").text("Wind Speed: " + data.wind.speed + " m/s, Direction: " + data.wind.deg + "°");
@@ -60,10 +62,75 @@ function renderButtons() {
 
 renderButtons();
 
+function futureForecast(city) {
+    $("#forecast").empty();
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=metric";
+
+    fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        //want to use long/lat of city to fetch info on city
+        console.log(data);
+                // H2 for 5-Day Forecast
+                
+                // P for description, temp, wind, humidity
+
+    })
+};
+
+// Save user input using local storage
+// Regenerate persistent data using local storage
+// Implement bootstrap - html header
+// Implement bootstrap - buttons
+// Implement bootstrap - form & search
+// Implement bootstrap - cards (+ new 5 days cards)
+//  README File
+
+
+
 $("#search-button").on("click", function (event) {
     event.preventDefault();
     var textInput = cityInput.val();
     cities.push(textInput);
     renderButtons();
     displayCityInfo(textInput);
+    futureForecast();
 });
+
+
+
+/* save button
+function handleSaveEntry(event) {
+    var saveButton = $(event.currentTarget);
+    var blockID = saveButton.closest("tr").attr("id");
+
+    var userEntry = $.trim(
+        $(event.currentTarget)
+            .closest("tr")
+            .find("textarea")
+            .val()
+    );
+
+    blockID = parseInt(blockID);
+    savedInput[blockID] = userEntry;
+    localStorage.setItem("userInput", JSON.stringify(savedInput));
+    if (!isNaN(blockID) && blockID >= 8 && blockID <= 18) {
+}
+}
+*/
+
+/* local storage check
+var workDay = JSON.parse(localStorage.getItem("userInput"));
+if (workDay) {
+    savedInput = workDay;
+} else {
+    savedInput = [];
+}
+*/
+
+/* DAYJS NOTES
+var today = dayjs().format("dddd DD MMMM YYYY");
+$("#currentDay").text(today);
+*/
